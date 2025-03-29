acae2e23523ad14090868d8abf2956f345cc1b7f
Nguyễn Khắc Nghiêm <82279566+NguyenKhacNghiem@users.noreply.github.com>
2025-03-29 15:24:39 +0700

    Update main.js in PDFKit

--- Web/NodeJS/PDFKit/main.js
+++ Web/NodeJS/PDFKit/main.js
@@ -3,8 +3,9 @@ PDFDocument = require('pdfkit');
 function createPDF(dataCallback, endCallback) {
     let pdf = new PDFDocument();
     
-    pdf.on('data', dataCallback);
-    pdf.on('end', endCallback);
+    pdf.on('data', dataCallback); // Mỗi khi có một phần dữ liệu PDF được tạo, nó sẽ gọi hàm dataCallback
+    pdf.on('end', endCallback);   // Khi file PDF hoàn thành, hàm endCallback sẽ được gọi.
+
     pdf.fontSize(25)
     pdf.text('Hello World', 100, 100);
     
