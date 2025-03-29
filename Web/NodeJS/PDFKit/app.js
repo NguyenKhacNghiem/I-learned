8b9a494e8438d6603f69ba0d99ff803ef3dfd44f
Nguyễn Khắc Nghiêm <82279566+NguyenKhacNghiem@users.noreply.github.com>
2025-03-29 15:25:06 +0700

    Update app.js in PDFKit

--- Web/NodeJS/PDFKit/app.js
+++ Web/NodeJS/PDFKit/app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
+
 const pdf = require(__dirname + '/main.js');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    let stream = res.writeHead(200, {
+    // Hiển thị file PDF trực tiếp trên trình duyệt
+    res.writeHead(200, {
         'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment; filename="my-file.pdf"'
+        'Content-Disposition': 'inline; filename="my-file.pdf"' // inline -> attachment để tự động tải xuống
     });
 
     pdf.createPDF(
-        data => stream.write(data), 
-        () => stream.end()
+        data => res.write(data), // Gửi dữ liệu PDF trực tiếp về client
+        () => res.end()          // Kết thúc response khi hoàn thành
     );
 });
 
-app.listen(3000, () => console.log('Download pdf file at http://localhost:3000'));
+app.listen(3000, () => console.log('Visit http://localhost:3000'));
